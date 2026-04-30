9e5e179022057517e6374b6a51552de6f98b4fd0
Uday-Choudhary <choudharyuday85@gmail.com>
2026-04-30 23:08:06 +0530

    assignment-1

--- Phase 1/assignment1.py
+++ Phase 1/assignment1.py
@@ -6,13 +6,14 @@ image_path = Path(__file__).parent / "minions.jpg"
 image = cv2.imread(str(image_path))
 
 if image is not None:
-    grey_image = cv2.cvtColor(image , cv2.COLOR_BGR2GRAY)
-
-    cv2.imshow('minion',grey_image)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
-
-    cv2.imwrite('grey_minion.png' , grey_image)
+    grey_image = cv2.cvtColor(image , cv2.COLOR_BGR2GRAY)  
+    choice = input('Save or Show: ')
+    if choice == 'Show':
+        cv2.imshow('minion',grey_image)
+        cv2.waitKey()
+        cv2.destroyAllWindows()
+    elif choice == 'Save':
+        cv2.imwrite(input('Enter the filename to save the grey image: ') + '.png', grey_image)
     print('all done')
 else:
     print('Error')
